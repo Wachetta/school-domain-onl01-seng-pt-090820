2026d9705506df9aef96842b7057c1a69928d7f9
Zachary Darneille <zakdarn@gmail.com>
2020-09-25 02:36:38 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -8,11 +8,11 @@ class School
   end
   
   def add_student(student_name, grade)
-    @roster[grade]
-   if @roster[grade]
+    if @roster[grade]
     @roster[grade] << student_name
   else
-   @roster[grade]
+   @roster[grade] = []
+   @roster[grade] << student_name
    end
   end
   
